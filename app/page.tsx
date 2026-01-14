9f160eebef75c483d012841d041977a4ea290250
bisht-xp <bishtkamal2907@gmail.com>
2026-01-14 18:07:54 +0530

    move header and warnign sign to layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,6 @@ import TableComponent from "./components/table-component/TableComponent";
 export default function Home() {
   return (
     <div className="min-h-screen overflow-x-hidden">
-      <Header />
-      <Warning />
       <TableComponent />
     </div>
   );
